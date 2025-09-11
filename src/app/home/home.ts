755e185f3583135ac830edbf95e11818a636caed
kareem mahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2025-09-11 15:10:57 +0300

    skills init & its lazy routes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Projects } from "../shared/projects/projects";
+import { Skills } from "../shared/skills/skills";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Projects],
+  imports: [Projects, Skills],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
