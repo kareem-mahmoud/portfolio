c5b9257a799d99b5d46320dffe7f4454300988c5
kareem mahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2025-09-15 15:11:48 +0300

    Footer updates

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { nav } from '../../module/app-module';
+import { ContactForm } from "../contact-form/contact-form";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ContactForm],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
@@ -15,12 +16,13 @@ export class Footer {
     { title: 'Kareem Mahmoud' },
     { email: 'Kareem.mahmoud.abd.elhannan@gmail.com' },
     { paragraph: 'Dedicated senior UI Developerwith 8+ years of experience.'},
-    { info: 'Senior UI Developer | Front end (Angular) | WordPress Developer UI & Graphic Designer.'},
+    { info: 'Senior UI Developer | Front end (Angular) | WordPress Developer | UI & Graphic Designer.'},
     { logo: 'km.png'},
     { social: [
       { icon: 'linkedin.svg', url: 'https://www.linkedin.com/in/kareem-elbeltagy', name: 'LinkedIn' },
       { icon: 'github.svg', url: 'https://github.com/Kareem-Mahmoud' , name: 'Github'},
-    ]}
+    ]},
+    { hireMe: "Ready to bring your projects to life? Click the button below to hire me or inquire about my services. Let's discuss how I can help you achieve your goals."}
     
   ]; 
 
