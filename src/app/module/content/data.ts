02bd7e475f0b0d28263445ade7a841c776c11066
kareem mahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2026-04-02 17:59:37 +0200

    data

--- src/app/module/content/data.ts
+++ src/app/module/content/data.ts
@@ -16,7 +16,7 @@ export const ComponentsData = [
             firstContentH1B: 'Include 2+ [Angular] front-end based in cairo, Egypt.',
             firstContentP: 'He crafts responsive websites where technologies meet creativity.',
             firstContentButtonText: 'Contact me',
-            firstContentButtonURL: 'contact',
+            firstContentButtonURL: 'contacts',
         },
         aboutMe: {
             aboutMeDisc: aboutMe,
