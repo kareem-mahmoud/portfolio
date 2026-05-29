43a329c33c74c8d6cb47120375343d171385085c
kareem mahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2026-05-29 20:45:19 +0300

    create new component all skills

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,6 +23,11 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/single-project/single-project').then(m => m.SingleProject) 
     },
     { 
+        title: 'All Skills',
+        path: 'all-skills', 
+        loadComponent: () => import('./pages/all-skills/all-skills').then(m => m.AllSkills) 
+    },
+    { 
         title: 'Contact',
         path: 'contact', 
         loadComponent: () => import('./pages/contacts/contacts').then(m => m.Contacts)
