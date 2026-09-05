bc9eb9f75f583d34c68b1135e749103bc35f2af2
kareem-mahmoud <kareem.mahmoud.abd.elhannan@gmail.com>
2026-09-05 22:18:03 +0300

    TS icon

--- src/app/shared/reusable/skill-icon/skill-icon.ts
+++ src/app/shared/reusable/skill-icon/skill-icon.ts
@@ -14,6 +14,7 @@ export class SkillIcon {
     const normalizedName = name.toLowerCase();
 
     if (['js', 'javascript'].includes(normalizedName)) return 'JS';
+    if (['ts', 'typescript'].includes(normalizedName)) return 'TypeScript';
     if (['wordpress', 'wordpress.org'].includes(normalizedName)) return 'WordPress';
     if (['react', 'react.js', 'reactjs'].includes(normalizedName)) return 'React';
 
